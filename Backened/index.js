15b70ce633cc5f8c955e75f4f275779ffe4e601c
Harssh Ssarma <harssh.ssarma@gmail.com>
2024-11-13 01:41:40 +0530

    Update index.js

--- Backened/index.js
+++ Backened/index.js
@@ -12,38 +12,13 @@ const mongoose = require('mongoose');
 // Use environment variable for PORT
 const PORT = process.env.PORT || 8080;
 
-const allowedOrigins = [
-  'http://localhost:5173', // Local development
-  'https://chat-app-front-kappa.vercel.app', // Production frontend on Vercel
-];
-
 app.use(cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true, // Necessary for handling cookies and credentials
+    origin: ["https://chat-app-front-kappa.vercel.app"],
+    methods: ["POST", "GET", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
+    credentials: true,
+    optionsSuccessStatus: 200
 }));
-
-const { Server } = require("socket.io");
-const io = new Server(httpServer, {
-  cors: {
-    origin: allowedOrigins,
-    methods: ["GET", "POST"],
-    credentials: true, // Allows cookies and credentials to be sent
-  },
-});
-
-// app.use(cors({
-//     origin: ["https://chat-app-front-kappa.vercel.app"],
-//     methods: ["POST", "GET", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
-//     credentials: true,
-//     optionsSuccessStatus: 200
-// }));
 
 mongoose.connect('mongodb+srv://mohitkeshari2000:mohit123@chat-app.l5erv.mongodb.net/?retryWrites=true&w=majority&appName=Chat-App');
 
